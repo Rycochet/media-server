773b2b96ea80a2462a4d04387bd24a027e234d42
Ryc O'Chet <rycochet@rycochet.com>
2025-03-08 11:15:13 +0000

    Basic setup

---
+++ onlyfans/update_login.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Instructions:"
+echo "1. Choose -> Edit auth.json"
+echo "2. Choose -> Paste from Cookie Helper Script"
+echo "3. Actually paste contents"
+echo "4. Press Alt+Enter"
+echo "5. Choose -> Quit"
+echo ""
+
+docker compose run onlyfans ofscraper

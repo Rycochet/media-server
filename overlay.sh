2af2fa4717e14a0fc0c3e3e35c0b948ad561bde7
Ryc O'Chet <rycochet@rycochet.com>
2025-08-26 13:38:36 +0100

    Add GPU option

--- overlay.sh
+++ overlay.sh
@@ -34,11 +34,25 @@ unmount () {
     done
 }
 
+refresh () {
+    for i in "${arr[@]}"; do
+        declare ROOT=$(dirname "$i")
+        declare BASENAME=$(basename "$i")
+        declare MERGED=${ROOT}/.overlay/${BASENAME%/}
+
+        echo sudo mount -o remount "${MERGED}"
+        sudo mount -o remount "${MERGED}"
+    done
+    sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+}
+
 if [ "$1" == "up" ]; then
     mount
 elif [ "$1" == "down" ]; then
     unmount
+elif [ "$1" == "refresh" ]; then
+    refresh
 else
     echo "Usage:"
-    echo "$(basename $0) up|down"
+    echo "$(basename $0) up|down|refresh"
 fi

2447512f68c30e5f57ef00ad7e89d79ece5bcbb5
Ryc O'Chet <rycochet@rycochet.com>
2025-09-01 12:15:09 +0100

    Update cron job script

--- minimum-keep-alive.sh
+++ minimum-keep-alive.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # This script should be called every minute from cron - this ensures that the minimum
-# routing exists, even if no servers are running.
+# routing exists, even if no servers are running:
+
+# * * * * * /bin/bash /path/to/minimum-keep-alive.sh 2>&1 | /usr/bin/logger -t minimum-keep-alive
 
 if [ ! -n "$(docker ps --filter "name=traefik" --filter "status=running" --quiet)" ]; then
     # Make sure we have the right compose file path relative to this script
-    cd "$(dirname "$(dirname "$(realpath $0)")")"
+    cd "$(dirname "$(realpath $0)")"
     docker compose up -d traefik
 fi

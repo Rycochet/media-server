5d6a1f2378228da00c1b7db575882f583e89837c
Ryc O'Chet <rycochet@rycochet.com>
2025-10-06 11:14:34 +0100

    Update minimum-keep-alive.sh

--- minimum-keep-alive.sh
+++ minimum-keep-alive.sh
@@ -5,8 +5,9 @@
 
 # * * * * * /bin/bash /path/to/minimum-keep-alive.sh 2>&1 | /usr/bin/logger -t minimum-keep-alive
 
+# Make sure we have the right compose file path relative to this script
+cd "$(dirname "$(realpath $0)")"
+
 if [ ! -n "$(docker ps --filter "name=traefik" --filter "status=running" --quiet)" ]; then
-    # Make sure we have the right compose file path relative to this script
-    cd "$(dirname "$(realpath $0)")"
     docker compose up -d traefik
 fi

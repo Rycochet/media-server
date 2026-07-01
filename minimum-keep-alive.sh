78cc79565980a0972bd04a96819ef32b07f765ca
Ryc O'Chet <rycochet@rycochet.com>
2026-07-01 10:20:53 +0100

    Fix minimum-keep-alive to survive a crashed socket-proxy

--- minimum-keep-alive.sh
+++ minimum-keep-alive.sh
@@ -8,6 +8,16 @@
 # Make sure we have the right compose file path relative to this script
 cd "$(dirname "$(realpath $0)")"
 
-if [ ! -n "$(docker ps --filter "name=traefik" --filter "status=running" --quiet)" ]; then
-    docker compose up -d traefik
+if [ -f .env.keep-alive ]; then
+    source .env.keep-alive
 fi
+
+if [[ -v KEEP_ALIVE ]] || [[ "$(declare -p KEEP_ALIVE)" =~ "declare -a" ]] || [ "${#KEEP_ALIVE[@]}" = "0" ]; then
+    KEEP_ALIVE=("socket-proxy" "tinyauth" "error-pages" "traefik")
+fi
+
+for i in "${KEEP_ALIVE[@]}"; do
+    if [ ! -n "$(docker ps --filter "name=$i" --filter "status=running" --quiet)" ]; then
+        docker compose up -d $i
+    fi
+done
